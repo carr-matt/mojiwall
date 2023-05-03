4653872587da3bc25cc1f187e8a3430e1efe8c80
Matt <carr.matthew.lee@gmail.com>
2023-05-03 14:51:21 -0500

    clerk UserButton component implemented for acct mgmt, post icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import { SignInButton, useUser } from "@clerk/nextjs";
+import { SignInButton, UserButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import { api } from "~/utils/api";
-import Image from "next/image";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { PageLayout } from "~/components/layout";
 import { PostView } from "~/components/postview";
+import { IoSend } from "react-icons/io5";
 
 const CreatePost = () => {
   const { user } = useUser();
@@ -34,12 +34,15 @@ const CreatePost = () => {
 
   return (
     <div className="flex w-full gap-3">
-      <Image
-        src={user.profileImageUrl}
-        alt="Profile image"
-        className="h-14 w-14 rounded-full"
-        width={56}
-        height={56}
+      <UserButton
+        appearance={{
+          elements: {
+            userButtonAvatarBox: {
+              width: 56,
+              height: 56,
+            },
+          },
+        }}
       />
       <input
         placeholder="Add your emojis!"
@@ -58,7 +61,9 @@ const CreatePost = () => {
         disabled={isPosting}
       />
       {input !== "" && !isPosting && (
-        <button onClick={() => mutate({ content: input })}>Post</button>
+        <button onClick={() => mutate({ content: input })}>
+          <IoSend size={38} />
+        </button>
       )}
       {isPosting && (
         <div className="flex items-center justify-center">
@@ -118,7 +123,9 @@ const Home: NextPage = () => {
       <div className="flex border-b border-slate-600 p-4">
         {!isSignedIn && (
           <div className="flex justify-center">
-            <SignInButton />
+            <SignInButton mode="modal" redirectUrl="window.location.href">
+              <button className="btn">Sign in</button>
+            </SignInButton>
           </div>
         )}
         {isSignedIn && <CreatePost />}
