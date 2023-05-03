d7e2422fc474c984c2ff6ad77a75b916d7c938c2
Matt <carr.matthew.lee@gmail.com>
2023-05-03 11:58:36 -0500

    typo (type-check -> typecheck), error msg info

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -25,7 +25,7 @@ const addUserDataToPosts = async (posts: Post[]) => {
     if (!author || !author.username)
       throw new TRPCError({
         code: "INTERNAL_SERVER_ERROR",
-        message: "Author for post not found",
+        message: `Author for post not found. POST ID: ${post.id}, USER ID: ${post.authorId}`,
       });
 
     return {
