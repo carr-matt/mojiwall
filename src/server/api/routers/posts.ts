f3113e9411f2f49d6a30c566790a9af6b3d8856a
Matt <carr.matthew.lee@gmail.com>
2023-05-03 13:52:10 -0500

    added footer to layout, author not found error

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -2,7 +2,7 @@ import type { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
-import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 import {
   createTRPCRouter,
@@ -10,40 +10,36 @@ import {
   publicProcedure,
 } from "~/server/api/trpc";
 import type { Post } from "@prisma/client";
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 const addUserDataToPosts = async (posts: Post[]) => {
+  const userId = posts.map((post) => post.authorId);
   const users = (
     await clerkClient.users.getUserList({
-      userId: posts.map((post) => post.authorId),
-      limit: 100,
+      userId: userId,
+      limit: 110,
     })
   ).map(filterUserForClient);
 
   return posts.map((post) => {
     const author = users.find((user) => user.id === post.authorId);
 
-    if (!author || !author.username)
+    if (!author) {
+      console.error("AUTHOR NOT FOUND", post);
       throw new TRPCError({
         code: "INTERNAL_SERVER_ERROR",
         message: `Author for post not found. POST ID: ${post.id}, USER ID: ${post.authorId}`,
       });
+    }
 
     return {
       post,
       author: {
         ...author,
-        username: author.username,
+        username: author.username ?? "(username not found)",
       },
     };
   });
-};
-
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profileImageUrl: user.profileImageUrl,
-  };
 };
 
 // Create a new ratelimiter, that allows 3 requests per 1 minute
